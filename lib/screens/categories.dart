2df6a858220bce77147ccf27aae6ff49d488d7e7
Rudolfo Lange Neto <rlneto@live.com>
2025-05-27 13:10:09 -0300

    Feature> Mostrando as refeições (dummy data)

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:refeicoes_flutter/data/dummy_data.dart';
+import 'package:refeicoes_flutter/models/category.dart';
 import 'package:refeicoes_flutter/screens/meals.dart';
 import 'package:refeicoes_flutter/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals =
+        dummyMeals
+            .where((meal) => meal.categories.contains(category.id))
+            .toList();
+
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (ctx) => MealsScreen(title: 'title', meals: []),
+        builder:
+            (ctx) => MealsScreen(title: category.title, meals: filteredMeals),
       ),
     );
   }
@@ -32,7 +39,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectCategory: () {
-                _selectCategory(context);
+                _selectCategory(context, category);
               },
             ),
         ],
