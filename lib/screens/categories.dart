5b3ab02e1ca89b3c8fb1449c6bc16e187af638f3
Rudolfo Lange Neto <rlneto@live.com>
2025-05-27 11:25:16 -0300

    Tela inicial com dummy data.

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:refeicoes_flutter/data/dummy_data.dart';
+import 'package:refeicoes_flutter/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -8,16 +10,17 @@ class CategoriesScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("Escolha uma categoria")),
       body: GridView(
+        padding: EdgeInsets.all(16),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: List.generate(
-          6,
-          (i) => Text("Item $i", style: TextStyle(color: Colors.white)),
-        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category),
+        ],
       ),
     );
   }
